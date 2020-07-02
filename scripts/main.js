0eb9941b92d38b4c5dd8c0f4a34df7a4119cb150
Nathan Y <NDY1981@users.noreply.github.com>
2020-07-02 00:18:15 -0700

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+let checkTotal = document.getElementById("checkTotal");
+let tipPercentage = document.getElementById("tipPercentage");
+let tipPercentageOutput = document.getElementById("tipPercentageOutput");
+let total = document.getElementById("total");
+let tip = document.getElementById("tip");
+let totalWithTip = document.getElementById("totalWithTip");
+
+
+tipPercentage.oninput = function() {
+    tipPercentageOutput.textContent = tipPercentage.value;
+    tip.textContent = (checkTotal.value * (tipPercentage.value / 100)).toFixed(2);
+    totalWithTip.textContent = (parseFloat(total.textContent) + parseFloat(tip.textContent)).toFixed(2);
+}
+
+checkTotal.oninput = function() {
+    total.textContent = (parseFloat(checkTotal.value)).toFixed(2);
+    tip.textContent = (checkTotal.value * (tipPercentage.value / 100)).toFixed(2);
+    totalWithTip.textContent = (parseFloat(total.textContent) + parseFloat(tip.textContent)).toFixed(2);
+}
